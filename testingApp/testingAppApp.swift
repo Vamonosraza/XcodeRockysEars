82b65cbb616af5f3b55aae04cbc22ad1bd87322c
Vamonosraza <martinezjessy393@gmail.com>
2025-03-04 13:32:10 -0800

    Initial Commit

---
+++ testingApp/testingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingAppApp.swift
+//  testingApp
+//
+//  Created by Jessy  Martinez  on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct testingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testingAppTests/testingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  testingAppTests.swift
+//  testingAppTests
+//
+//  Created by Jessy  Martinez  on 3/4/25.
+//
+
+import Testing
+@testable import testingApp
+
+struct testingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
